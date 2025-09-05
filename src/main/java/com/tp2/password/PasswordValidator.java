4417dba223bc167af284fc106244e038fa0c5462
ReemeerieLautaro <lreemeerie@mail.austral.edu.ar>
2025-09-05 08:44:57 -0300

    Update PasswordValidator.java

--- src/main/java/com/tp2/password/PasswordValidator.java
+++ src/main/java/com/tp2/password/PasswordValidator.java
@@ -1,16 +1,1 @@
-package com.tp2.password;
 
-import java.util.regex.Pattern;
-
-public class PasswordValidator {
-
-
-    private static final Pattern RULES = Pattern.compile(
-            "^(?=.[a-z])(?=.[A-Z])(?=.\\d)(?=.[!@#$%^&*()_+\\-=\\[\\]{};:'\",.<>/?]).{8,}$"
-    );
-
-    public boolean isValid(String password) {
-        if (password == null) return false;
-        return RULES.matcher(password).matches();
-    }
-}
